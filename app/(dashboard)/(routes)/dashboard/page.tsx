6f060a991c44423f63c726f3401ad9ab5945fd4c
shirko <shir617@gmail.com>
2023-11-05 14:30:08 -0500

    dashboard added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { MessageSquare } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "test-violet-500/10",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+];
+
 const DashboardPage = () => {
   return (
     <div>
-      <p>Dashboard(protected)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Generate content with GenerateAI. Experience the power of AI.
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"></Card>
+        ))}
+      </div>
     </div>
   );
 };
